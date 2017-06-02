21a17c12ab33bce4fe46fa3ab1bb4d9b859e8805
JedidiahBertram <jedidiah.bertram@gmail.com>
2017-06-02 11:59:42 -0500

    adds hit functionality, hit count, and game end condition.

--- public/main.js
+++ public/main.js
@@ -5,38 +5,53 @@ window.onload = function() {
   var container = document.getElementById('container');
   var container2 = document.getElementById('container2');
   var selectedColor = 'red';
-  var guessSquare = null;
+  var fireMissile = null;
+  var hitCount = 0;
   socket.on('userDivClick', function(msg) {
     console.log('test');
-    var squareAttack = document.getElementById(msg.square)
-    squareAttack.className += ' ship'
+    var opponentTarget = document.getElementById(msg.square)
+    opponentTarget.className += ' ship';
   });
 
   for (var i = 1; i <= 100; i++) {
-    let setSquare = document.createElement('div');
-    setSquare.className = 'square';
-    setSquare.id = `a${i}`;
-    setSquare.addEventListener('click', function(ev) {
-      var opposingSquareId = "b" + setSquare.id.slice(1, 4);
+    let shipSelectionDiv = document.createElement('div');
+    shipSelectionDiv.className = 'square';
+    shipSelectionDiv.id = `a${i}`;
+    shipSelectionDiv.addEventListener('click', function(ev) {
+      var opposingSquareId = "b" + shipSelectionDiv.id.slice(1, 4);
       socket.emit('userDivClick', {
         square: opposingSquareId
       });
       var ship = document.getElementById(opposingSquareId)
       console.log(ship);
-      setSquare.style.backgroundColor = '#FFFFF2';
+      shipSelectionDiv.style.backgroundColor = '#D3D3D3';
     })
 
-    container.append(setSquare);
+    container.appendChild(shipSelectionDiv);
   }
   for (var i = 1; i <= 100; i++) {
-    guessSquare = document.createElement('div');
-    guessSquare.className = 'square';
-    var squareClass = guessSquare.className
-    guessSquare.id = `b${i}`;
-    guessSquare.addEventListener('click', function(ev) {
-      console.log(squareClass)
-    })
+    fireMissile = document.createElement('div');
+    fireMissile.className = 'square';
+    //var squareClass = fireMissile.className
+    fireMissile.id = `b${i}`;
+    container2.appendChild(fireMissile);
+    let _fireMissile = document.getElementById(`b${i}`)
 
-    container2.append(guessSquare);
+    _fireMissile.addEventListener('click', function(ev) {
+      console.log(_fireMissile);
+      console.log(_fireMissile.className)
+      if (_fireMissile.className == 'square ship') {
+        console.log(true);
+        _fireMissile.className = 'hitShip';
+        hitCount += 1;
+        console.log(hitCount);
+      } else {
+        _fireMissile.className = 'miss';
+      }
+      if (hitCount > 16) {
+        window.alert("Game over. Someone won. BE HONEST!")
+        console.log('Someone won. I dont know who. But Someone won.')
+      }
+    })
   }
 };
